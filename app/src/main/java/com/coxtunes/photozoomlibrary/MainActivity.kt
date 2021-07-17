84b3f11fcf64bdb24a98d603a87e966ebd026dfa
Nayeem Shiddiki Abir <abircoxsbazar@gmail.com>
2021-07-17 13:47:52 +0600

    Update MainActivity.kt
    Online Image load example added

--- app/src/main/java/com/coxtunes/photozoomlibrary/MainActivity.kt
+++ app/src/main/java/com/coxtunes/photozoomlibrary/MainActivity.kt
@@ -10,5 +10,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val photoview = findViewById(R.id.photo_view) as PhotoView
         photoview.setImageResource(R.drawable.image)
+        
+        // For load image from url of api you can user like this (here image URI is url)
+          Picasso.get().load(imageUrl)
+                    .placeholder(R.drawable.errorplaceholder)
+                    .into(binding.photoView)
     }
 }
